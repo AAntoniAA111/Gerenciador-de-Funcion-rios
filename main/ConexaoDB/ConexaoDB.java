44af053d7d6fbcdcb30e80934b39d34773a59571
AntoniaQ <quintinodev01@gmail.com>
2026-05-19 11:36:28 -0300

    Update ConexaoDB.java

--- main/ConexaoDB/ConexaoDB.java
+++ main/ConexaoDB/ConexaoDB.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 class ConexaoDB {
-    private static final String URL = "jdbc:mysql://localhost:3306/Geremciamentodefuncionarios";
+    private static final String URL = "jdbc:mysql://localhost:3306/gerenciador_funcionarios";
     private static final String USER = "root";
-    private static final String PASSWORD = "W444W43J03Q";
+    private static final String PASSWORD = "";
 
     public static Connection getConexao() {
         try {
