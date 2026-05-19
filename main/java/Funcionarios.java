b2aa233ad86c7185172721760b07b16031ca25d0
AntoniaQ <quintinodev01@gmail.com>
2026-05-19 18:43:11 -0300

    Add files via upload

---
+++ main/java/Funcionarios.java
@@ -0,0 +1,43 @@
+public abstract class Funcionarios {
+    private int id;
+    private String nome;
+    private String cpf;
+    private double salarioBase;
+
+    public Funcionarios(String nome, String cpf, double salarioBase){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public double getSalarioBase(){
+        return salarioBase;
+    }
+    public void setSalarioBase(double salarioBase){
+        if(salarioBase > 0){
+            this.salarioBase = salarioBase;
+        } else {
+            System.out.println("Salário inválido!");
+        }
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    //METODO ABSTRATO
+    public abstract double calcularSalario();
+
+    public void exibirInfo(){
+        System.out.printf("Nome: " + nome);
+        System.out.println("Salário: " + calcularSalario());
+    }
+}
